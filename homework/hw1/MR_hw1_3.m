4814fcb69c406a75561bc52ac76149532541268b
gkiar <gkiar07@gmail.com>
2016-02-06 14:47:30 -0500

    updated homework and included combined phase

--- homework/hw1/MR_hw1_3.m
+++ homework/hw1/MR_hw1_3.m
@@ -3,14 +3,14 @@ close all
 load('whatisthis_8coil')
 
 %% part 1
-figure
+figure(1)
 for i=1:size(data,3)
     subplot(3,3,i)
     imagesc(log(abs(data(:,:,i))+1))
     colormap(gray); axis off;
 end
 
-figure
+figure(2)
 for i=1:size(data,3)
     subplot(3,3,i)
     imagesc(angle(data(:,:,i)))
@@ -22,11 +22,20 @@ end
 im = fftshift(ifft2(ifftshift(data)));
 im_rss = sqrt(sum(abs(im).^2, 3));
 
-figure
-subplot(2,1,1)
-imagesc(log(im_rss+1)); axis off; axis equal
-colormap(gray)
+figure(3)
+subplot(221)
+imagesc(log(abs(im(:,:,1))+1)); axis off; axis equal
+colormap(gray); title('Single coil magnitude image')
 
-subplot(2,1,2)
+subplot(222)
+imagesc(log(im_rss+1)); axis off; axis equal
+colormap(gray); title('Reconstructed magnitude image')
+
+subplot(223)
 imagesc(angle(im(:,:,1))); axis off; axis equal
-colormap(gray)
+colormap(gray); title('Single coil phase image')
+
+angi = abs(acos(sum(abs(data).*cos(angle(data)),3)./im_rss));
+subplot(224)
+imagesc(angi); axis off; axis equal
+colormap(gray); title('Reconstructed phase image')
