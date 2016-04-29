9bbdc21565b9a0f30e591045446fe3b7b834fd78
gkiar <gkiar07@gmail.com>
2016-04-29 00:02:44 -0400

    updated fig and code

--- homework/hw4/code/part2.m
+++ homework/hw4/code/part2.m
@@ -1,7 +1,6 @@
 load('mod_raw_025_NOI.mat')
 
 figure
-
 for i = 1:6
    subplot(2,3,i)
    dat = noise_data(:,1,1,i);
@@ -19,13 +18,15 @@ end
 
 flat = [real(noise_data); imag(noise_data)];
 figure
+mystd = [];
 for i = 1:6
     subplot(2, 3, i)
     hist(flat(:,1,1,i), 10000)
+    mystd(i) = std(flat(:,1,1,i));
     xlim([-30 30])
 end
 
-fift = fft(noise_data);
+fift = fft(noise_data)/length(noise_data);
 fiftflat = [real(fift); imag(fift)]/250;
 figure
 for i = 1:6
@@ -35,11 +36,23 @@ end
 
 magfitty = RSS(fift);
 figure
+xbar = {};
+sigma = {};
 for i =1:6
     subplot(3, 3, i)
-    hist(RSS(fift(:,:,1, i)), 10000)
-    xlabel(strcat('Mean:',num2str(mean(RSS(fift(:,:,1,i)))),'; std:',num2str(std(RSS(fift(:,:,1,i))))))
+    d = RSS(fift(:,:,1, i));
+    hist(d, 10000)
+    xbar{i} = mean(d);
+    sigma{i} = std(d);
+    xlabel(strcat('Mean:',num2str(xbar{i}),'; std:',num2str(sigma{i})))
 end
 subplot(3,3,9)
 hist(magfitty, 10000)
 xlabel(strcat('Mean:',num2str(mean(magfitty)),'; std:',num2str(std(magfitty))))
+
+
+mbar = @(sig) sig.*sqrt(pi/2);
+sigbar = @(sig) (2 - pi/2).*sig.^2;
+
+[xbar', sigma']
+[mbar(mystd)', sigbar(mystd)']
